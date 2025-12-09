d98f6f8d0a5db999fb74c7deb2b405f3bb4245b8
david-hurley <hurleyldave@gmail.com>
2025-12-08 18:03:41 -0700

    new spatial

---
+++ water-well-spatial-explorer-app/config.py
@@ -0,0 +1,6 @@
+# PostgreSQL Configuration
+instance_name = "alberta-well-water-demo"
+database = "users"
+schema = "david_hurley"
+well_table = "alberta_wdrill_wells_pg"
+lithology_table = "alberta_wdrill_combined_location_lithology_p"
